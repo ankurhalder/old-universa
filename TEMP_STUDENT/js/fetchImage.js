d44cf0e31a94ef0563c2801b9a12e305fe5dcb38
Ankur Halder <ankur.halder12345@gmail.com>
2023-04-05 18:06:00 +0530

    image is fetching

---
+++ TEMP_STUDENT/js/fetchImage.js
@@ -0,0 +1,20 @@
+function fetchImage() {
+    const Token = localStorage.getItem("token");
+    fetch(`https://universa-api.onrender.com/uploads/images/profile`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "token": Token,
+        "profile_image_name": Token
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        const img = document.getElementById("user-image");
+        img.src = data.data.files[0].url;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+  
