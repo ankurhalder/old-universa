d68dd84d02e98a5d07b431bea31e5a78e34ac06a
Ankur <ankur.halder12345@gmail.com>
2023-04-09 23:37:14 +0530

    bug

--- ADMIN/js/getAllPermanent.js
+++ ADMIN/js/getAllPermanent.js
@@ -1,28 +1,45 @@
-function getAllPermanent() {
-  fetch(`https://universa-api.onrender.com/student`, {
-    method: "GET",
-    credentials: "include",
-    headers: {
-      "token": localStorage.getItem("token"),
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      Applicants = document.getElementById("applicant");
-      if (data.status === true) {
-        console.log("Permanent Account Received");
-        const dataCount = data.data.length;
-        console.log(dataCount);
-        console.log("hello world");
-        for (let i = 0; i < dataCount; i++) {
-          const pTag = document.createElement("p");
-          pTag.textContent = data.data[i].course_info.enrollment_number;
-          Applicants.appendChild(pTag);
-        }
-      } else if (data.status === false) {
-        alert(`unsuccessful`);
-      }
+// function getAllPermanent() {
+//   fetch(`https://universa-api.onrender.com/student`, {
+//     method: "GET",
+//     credentials: "include",
+//     headers: {
+//       "token": localStorage.getItem("token"),
+//     },
+//   })
+//     .then((response) => response.json())
+//     .then((data) => {
+//       console.log(data);
+//       Applicants = document.getElementById("applicant");
+//       if (data.status === true) {
+//         console.log("Permanent Account Received");
+//         const dataCount = data.data.length;
+//         console.log(dataCount);
+//         console.log("hello world");
+//         for (let i = 0; i < dataCount; i++) {
+//           const pTag = document.createElement("p");
+//           pTag.textContent = data.data[i].course_info.enrollment_number;
+//           Applicants.appendChild(pTag);
+//         }
+//       } else if (data.status === false) {
+//         alert(`unsuccessful`);
+//       }
+//     })
+//     .catch((error) => console.error(error));
+// }
+// flwibhflweifblw
+
+async function getAllPermanent() {
+  try {
+    const response = await  fetch(`https://universa-api.onrender.com/student`, {
+      method: "GET",
+      credentials: "include",
+      headers: {
+        "token": localStorage.getItem("token"),
+      },
     })
-    .catch((error) => console.error(error));
+    const data = await response.json()
+    console.log(data)
+  } catch (error) {
+    
+  }
 }
